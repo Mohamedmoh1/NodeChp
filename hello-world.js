0a5f4eaabc859fbc06ec2ed542e66642fad6b936
Mohamedmoh1 <med24795@gmail.com>
2024-01-04 10:05:32 +0100

    NodeChp

---
+++ hello-world.js
@@ -0,0 +1,11 @@
+//console.log('Hello World')
+
+var generator = require('generate-password');
+
+var password = generator.generate({
+	length: 10,
+	numbers: true
+});
+
+// 'uEyMTw32v9'
+console.log(password);
